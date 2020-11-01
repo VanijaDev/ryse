5c2c2f0bc9cd53b6fbb5c6c84b6e01ada16dc66f
Ivan Solomichev <vanijatko.social@gmail.com>
2020-11-01 10:59:30 +0200

    implemented: 1) bonding curve 2) buy, sell

--- contracts/MarketContract.sol
+++ contracts/MarketContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.0;
+pragma solidity ^0.7.0;
 
 import "./TestToken.sol";
 import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
@@ -19,115 +19,108 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
   * Market.setToken(_token)
  */
 contract MarketContract is Ownable {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    TestToken public token;
+  TestToken public token;
 
-    uint256 public solidTokenPrice = 500000000 gwei;    // 0.5 ETH
-    uint256 public tokensBought = 10;
+  uint256 public solidTokenPrice = 500000000 gwei;    // 0.5 ETH
+  uint256 public tokensBought = 10;
 
-    modifier onlyCorrectValueToBuyExactTokens(uint256 _tokens) {
-        require(msg.value == valueToBuyExactTokens(_tokens), "wrong buy value");
-        _;
-    }
+  modifier onlyMoreThanZero(uint256 _amount) {
+    require(_amount > 0, "must be > 0");
+    _;
+  }
 
-    modifier onlyZeroAddress(address _address) {
-        require(_address == address(0), "must be 0 addr");
-        _;
-    }
+  event TokensBought(address _purchaser, uint256 _amount, uint256 _value);
+  event TokensSold(address _seller, uint256 _amount, uint256 _value);
 
-    modifier onlyNonZeroAddress(address _address) {
-        require(_address != address(0), "must not be 0 addr");
-        _;
-    }
 
-    modifier onlyMoreThanZero(uint256 _amount) {
-        require(_amount > 0, "must be > 0");
-        _;
-    }
+  function setToken(TestToken _token) external onlyOwner {
+    require(address(_token) != address(0), "_token cannt be 0");
+    require(address(token) == address(0), "token is set");
 
-    event TokensBought(address _purchaser, uint256 _amount);
-    event TokensSold(address _seller, uint256 _amount);
+    token = _token;
+    require(token.balanceOf(address(this)) == 0x581767BA6189C400000, "wrong balance");
+  }
 
-    function setToken(TestToken _token) external onlyOwner onlyNonZeroAddress(address(_token)) onlyZeroAddress(address(token)) {
-        token = _token;
-        require(
-            token.balanceOf(address(this)) == 0x581767BA6189C400000,
-            "wrong balance"
-        );
-    }
+  /**
+    * @dev Buys tokens.
+    * @param _tokens Token amount to be bought.
+    */
+  function buyExactTokens(uint256 _tokens) public payable onlyMoreThanZero(_tokens) {
+    require(msg.value == valueToBuyExactTokens(_tokens), "wrong buy value");
 
-    /**
-      * @dev Buys tokens.
-      * @param _tokens Token amount to be bought.
-      */
-    function buyExactTokens(uint256 _tokens) public payable onlyMoreThanZero(_tokens) onlyCorrectValueToBuyExactTokens(_tokens) {
-        token.transfer(msg.sender, _tokens);
-        tokensBought = tokensBought.add(_tokens);
-        
-        emit TokensBought(msg.sender, _tokens);
-    }
+    token.transfer(_msgSender(), _tokens);
+    tokensBought = tokensBought.add(_tokens);
+    
+    emit TokensBought(_msgSender(), _tokens, msg.value);
+  }
 
-    /**
-      * @dev Calculates msg.value to be received for appropriate amount of bought tokens.
-      * @param _tokens Amount of tokens.
-      * @return msg.value to be received.
-      */
-    function valueToBuyExactTokens(uint256 _tokens) public view onlyMoreThanZero(_tokens) returns (uint256) {
-        uint256 lastPrice = priceForExactToken(tokensBought.add(_tokens));
-        return (priceForCurrentToken().add(lastPrice)).div(uint256(2)).mul(_tokens);
-    }
+  /**
+    * @dev Calculates msg.value to be received for appropriate amount of bought tokens.
+    * @param _tokens Amount of tokens.
+    * @return msg.value to be received.
+    */
+  function valueToBuyExactTokens(uint256 _tokens) public view onlyMoreThanZero(_tokens) returns (uint256) {
+    uint256 lastTokenPrice = priceForExactToken(tokensBought.add(_tokens));
+    return lastTokenPrice.sub(priceForCurrentToken());
+  }
 
-    /**
-      * @dev Sells tokens.
-      * @param _tokens Token amount to be sold.
-      */
-    function sellExactTokens(uint256 _tokens) public payable onlyMoreThanZero(_tokens) {
-        require(msg.value == valueToSellExactTokens(_tokens), "wrong msg.value");
-        tokensBought = tokensBought.minus(_tokens);
+  /**
+    * @dev Sells tokens.
+    * @param _tokens Token amount to be sold.
+    */
+  function sellExactTokens(uint256 _tokens) public onlyMoreThanZero(_tokens) {
+    uint256 transferValue = valueToSellExactTokens(_tokens);
+    require(balance() >= transferValue, "not enough balance");
+    require(token.allowance(_msgSender(), address(this)) >= _tokens, "amount not allowed");
+    
+    token.transferFrom(_msgSender(), address(this), _tokens);
+    tokensBought = tokensBought.sub(_tokens);
+    _msgSender().transfer(transferValue);
 
-        emit TokensSold(msg.sender, _tokens);
-    }
+    emit TokensSold(_msgSender(), _tokens, transferValue);
+  }
 
-    /**
-      * @dev Calculates value to be paid for appropriate amount of sold tokens.
-      * @param _tokens Amount of tokens.
-      * @return Value to be received.
-      */
-    function valueToSellExactTokens(uint256 _tokens) public view onlyMoreThanZero(_tokens) returns (uint256) {
-        uint256 lastPrice = priceForExactToken(tokensBought.minus(_tokens));
-        return (priceForCurrentToken().add(lastPrice)).div(uint256(2)).mul(_tokens);
-    }
+  /**
+    * @dev Calculates value to be paid for appropriate amount of sold tokens.
+    * @param _tokens Amount of tokens.
+    * @return Value to be received.
+    */
+  function valueToSellExactTokens(uint256 _tokens) public view onlyMoreThanZero(_tokens) returns (uint256) {
+    uint256 lastTokenPrice = priceForExactToken(tokensBought.sub(_tokens));
+    return priceForCurrentToken().sub(lastTokenPrice);
+  }
 
-    /**
-      * @dev Returns balance of the Smart Contract.
-      * @return Balance of the Smart Contract.
-      */
-    function balance() public view returns (uint256) {
-        return address(this).balance;
-    }
+  /**
+    * @dev Returns price for the next token.
+    * @return price for the next token.
+    */
+  function priceForCurrentToken() public view returns (uint256) {
+    return solidTokenPrice.mul(tokensBought.add(1)).div(1 ether);
+  }
 
-    /**
-      * @dev Returns price for the next token.
-      * @return price for the next token.
-      */
-    function priceForCurrentToken() public view returns (uint256) {
-        return solidTokenPrice.mul(tokensBought.add(1)).div(1 ether);
-    }
+  /**
+    * @dev Returns price for exact token.
+    * @param _token Exact token to get price for.
+    * @return Price for exact token.
+    */
+  function priceForExactToken(uint256 _token) public view returns (uint256) {
+    return solidTokenPrice.mul(_token).div(1 ether);
+  }
 
-    /**
-      * @dev Returns price for exact token.
-      * @param _token Exact token to get price for.
-      * @return Price for exact token.
-      */
-    function priceForExactToken(uint256 _token) public view returns (uint256) {
-        return solidTokenPrice.mul(_token).div(1 ether);
-    }
+  /**
+    * @dev Returns balance of the Smart Contract.
+    * @return Balance of the Smart Contract.
+    */
+  function balance() public view returns (uint256) {
+    return address(this).balance;
+  }
 
-    /**
-      * @dev Destroyes Smart Contract.
-      */
-    function kill() external onlyOwner {
-        selfdestruct(payable(owner()));
-    }
+  /**
+    * @dev Destroys Smart Contract.
+    */
+  function kill() external onlyOwner {
+    selfdestruct(payable(owner()));
+  }
 }
--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.7.0;
+pragma solidity ^0.7.0;
 
 import "../node_modules/@openzeppelin/contracts/GSN/Context.sol";
 import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
@@ -18,17 +18,16 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract TestToken is Context, IERC20 {
   using SafeMath for uint256;
 
-  mapping (address => uint256) private _balances;
+mapping (address => uint256) private _balances;
 
-  mapping (address => mapping (address => uint256)) private _allowances;
+mapping (address => mapping (address => uint256)) private _allowances;
 
-  uint256 private _totalSupply = 0x65A4DA25D3016C00000;  //  30,000
+uint256 private _totalSupply = 0x65A4DA25D3016C00000;  //  30,000
 
-  string constant private _name = "TokenName";
-  string constant private _symbol = "TTTT";
-  uint8 constant private _decimals = 18;
-  
-  
+string constant private _name = "TokenName";
+string constant private _symbol = "TTTT";
+uint8 constant private _decimals = 18;
+
   /**
     * @dev Smart Contract constructor.
     * @param _market Address of market Smart Contract.
@@ -36,18 +35,17 @@ contract TestToken is Context, IERC20 {
   constructor (address _market) {
     require(_market != address(0), "cannt be 0");
 
-    _balances[address(0xdD870fA1b7C4700F2BD7f44238821C26f7392148)] = 0x3635C9ADC5DEA00000;  //  team_1                == 1,000
-    _balances[address(0x583031D1113aD414F02576BD6afaBfb302140225)] = 0x3635C9ADC5DEA00000;  //  team_2                == 1,000
-    _balances[address(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB)] = 0x3635C9ADC5DEA00000;  //  team_3                == 1,000
-    _balances[address(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C)] = 0x3635C9ADC5DEA00000;  //  marketing             == 1,000
+    _balances[0xdD870fA1b7C4700F2BD7f44238821C26f7392148] = 0x3635C9ADC5DEA00000;  //  team_1                == 1,000
+    _balances[0x583031D1113aD414F02576BD6afaBfb302140225] = 0x3635C9ADC5DEA00000;  //  team_2                == 1,000
+    _balances[0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB] = 0x3635C9ADC5DEA00000;  //  team_3                == 1,000
+    _balances[0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C] = 0x3635C9ADC5DEA00000;  //  marketing             == 1,000
     _balances[_market] = 0x581767BA6189C400000;                                             //  market Smart Contract == 26,000
-    require(_balances[address(0xdD870fA1b7C4700F2BD7f44238821C26f7392148)]
-            .add(_balances[address(0x583031D1113aD414F02576BD6afaBfb302140225)])
-            .add(_balances[address(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB)])
-            .add(_balances[address(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C)])
+    require(_balances[0xdD870fA1b7C4700F2BD7f44238821C26f7392148]
+            .add(_balances[0x583031D1113aD414F02576BD6afaBfb302140225])
+            .add(_balances[0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB])
+            .add(_balances[0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C])
             .add(_balances[_market]) == 0x65A4DA25D3016C00000, "wrong balances");
   }
-
 
   /**
     * @dev Returns the name of the token.
@@ -197,12 +195,12 @@ contract TestToken is Context, IERC20 {
     * - `sender` must have a balance of at least `amount`.
     */
   function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-      require(sender != address(0), "ERC20: transfer from the zero address");
-      require(recipient != address(0), "ERC20: transfer to the zero address");
+    require(sender != address(0), "ERC20: transfer from the zero address");
+    require(recipient != address(0), "ERC20: transfer to the zero address");
 
-      _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-      _balances[recipient] = _balances[recipient].add(amount);
-      emit Transfer(sender, recipient, amount);
+    _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+    _balances[recipient] = _balances[recipient].add(amount);
+    emit Transfer(sender, recipient, amount);
   }
 
   /**
@@ -219,10 +217,10 @@ contract TestToken is Context, IERC20 {
     * - `spender` cannot be the zero address.
     */
   function _approve(address owner, address spender, uint256 amount) internal virtual {
-      require(owner != address(0), "ERC20: approve from the zero address");
-      require(spender != address(0), "ERC20: approve to the zero address");
+    require(owner != address(0), "ERC20: approve from the zero address");
+    require(spender != address(0), "ERC20: approve to the zero address");
 
-      _allowances[owner][spender] = amount;
-      emit Approval(owner, spender, amount);
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
   }
 }
