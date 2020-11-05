aa93bf895b5ba4630e860f99f1d098749d05ac80
Ivan Solomichev <vanijatko.social@gmail.com>
2020-11-05 12:38:45 +0200

    Comments added

--- contracts/IMilestone.sol
+++ contracts/IMilestone.sol
@@ -3,9 +3,18 @@
 pragma solidity ^0.7.0;
 
 abstract contract IMilestone {
+    /**
+     * @dev Implements functional to launch the milestone.
+     */
     function launchMilestone() public virtual;
 
+    /**
+     * @dev Implements functional to finish the milestone.
+     */
     function finishMilestone() public virtual;
 
+    /**
+     * @dev ITransfers token ownership after milestone finished.
+     */
     function transferTokenOwnership() internal virtual;
 }
